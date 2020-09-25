5324840eaed7e29f175df833ddff614c96a9c59b
sformichella <sformichella@gmail.com>
2020-09-25 10:53:42 -0700

    add subtract, multiply, and divide functionality

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-//Getting all those doc elements
 
-let firstNumber = document.getElementById('first-number');
-let secondNumber = document.getElementById('second-number');
-let calculateButton = document.getElementById('calculate-button');
-let result = document.getElementById('result');
+    // Addition Inputs
 
-//Button function. Outputs 'Please enter two numbers!' if at least one of the inputs is empty.
+let additionFirstNumber = document.getElementById('add-first-number');
 
-calculateButton.addEventListener('click', () => {
-    let number1 = firstNumber.value;
-    let number2 = secondNumber.value;
+let additionSecondNumber = document.getElementById('add-second-number');
+
+let sumButton = document.getElementById('sum-calc');
+
+let result = document.getElementById('sum');
+
+
+
+    // Addition Calc
+
+sumButton.addEventListener('click', () => {
+    let number1 = additionFirstNumber.value;
+    let number2 = additionSecondNumber.value;
     
     if (number1 === '' || number2 === '') {
         result.textContent = 'Please enter two numbers!'
@@ -17,5 +23,110 @@ calculateButton.addEventListener('click', () => {
     else {
         answer = Number(number1) + Number(number2);
         result.textContent = answer;
+    }
+})
+
+
+
+
+
+
+
+
+
+    // Subtract Inputs
+
+let subFirstNumber = document.getElementById('subtract-first-number');
+
+let subSecondNumber = document.getElementById('subtract-second-number');
+
+let differenceButton = document.getElementById('difference-calc');
+
+let difference = document.getElementById('difference');
+
+
+
+    // Subtract Calc
+
+differenceButton.addEventListener('click', () => {
+    let number1 = subFirstNumber.value;
+    let number2 = subSecondNumber.value;
+    
+    if (number1 === '' || number2 === '') {
+        difference.textContent = 'Please enter two numbers!'
+    }
+    else {
+        answer = Number(number1) - Number(number2);
+        difference.textContent = answer;
+    }
+})
+
+
+
+
+
+
+
+
+
+    // Multiply Inputs
+
+let multFirstNumber = document.getElementById('multiply-first-number');
+
+let multSecondNumber = document.getElementById('multiply-second-number');
+
+let productButton = document.getElementById('product-calc');
+
+let product = document.getElementById('product');
+
+
+
+    // Multiply Calc
+
+productButton.addEventListener('click', () => {
+    let number1 = multFirstNumber.value;
+    let number2 = multSecondNumber.value;
+    
+    if (number1 === '' || number2 === '') {
+        product.textContent = 'Please enter two numbers!'
+    }
+    else {
+        answer = Number(number1) * Number(number2);
+        product.textContent = answer;
+    }
+})
+
+
+
+
+
+
+
+
+
+    // Divide Inputs
+
+let divideFirstNumber = document.getElementById('divide-first-number');
+
+let divideSecondNumber = document.getElementById('divide-second-number');
+
+let quotientButton = document.getElementById('quotient-calc');
+
+let quotient = document.getElementById('quotient');
+
+
+
+    // Divide Calc
+
+quotientButton.addEventListener('click', () => {
+    let number1 = divideFirstNumber.value;
+    let number2 = divideSecondNumber.value;
+    
+    if (number1 === '' || number2 === '') {
+        quotient.textContent = 'Please enter two numbers!'
+    }
+    else {
+        answer = Number(number1) / Number(number2);
+        quotient.textContent = answer;
     }
 })
